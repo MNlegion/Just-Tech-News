b769b5f325a2a73e09b7c83b9719d2340aeb9f88
MNlegion <rkreuser30@gmail.com>
2022-05-30 19:43:50 -0500

    code refactor

--- models/index.js
+++ models/index.js
@@ -1,5 +1,6 @@
-const User = require('./User');
+// import all models
 const Post = require('./Post');
+const User = require('./User');
 const Vote = require('./Vote');
 const Comment = require('./Comment');
 
@@ -9,7 +10,7 @@ User.hasMany(Post, {
 });
 
 Post.belongsTo(User, {
-  foreignKey: 'user_id',
+  foreignKey: 'user_id'
 });
 
 User.belongsToMany(Post, {
